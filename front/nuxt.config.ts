9f33e62c8c521a57cdfda412071573fbbaf88613
RomainRodrigues <romain.rodrigues@etudiant.univ-rennes1.fr>
2024-02-02 16:19:26 +0100

    chore : update nuxt version

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,6 +1,12 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    ssr: false,
+    devtools: true,
+    css: ['~/assets/css/main.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
 })
